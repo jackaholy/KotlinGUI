508eb3745accc6dcb04dcdb6e301a5a88a3760d3
Jack Holy <jholy@carroll.edu>
2024-02-26 19:14:49 -0700

    Implement working volume change

--- main.kt
+++ main.kt
@@ -2,6 +2,8 @@ package com.zetcode
 
 import java.awt.EventQueue
 import javax.swing.JFrame
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class SimpleEx(title: String) : JFrame() {
 
@@ -26,5 +28,21 @@ private fun createAndShowGUI() {
 }
 
 fun main() {
+    // Create an instance of the GUI window
     EventQueue.invokeLater(::createAndShowGUI)
+    // Desired volume level (0-100)
+    val volume = 40
+    // Shell command to set the volume
+    val volumeProcess =
+        Runtime.getRuntime().exec(arrayOf("/usr/bin/osascript", "-e", "set volume output volume $volume"))
+    val volumeReader = BufferedReader(InputStreamReader(volumeProcess.inputStream))
+    var vol: String?
+    // Read command output
+    while (volumeReader.readLine().also { vol = it } != null) {
+        println(vol)
+    }
+    // Wait for the command to complete
+    volumeProcess.waitFor()
+
+    println("Volume set to $volume")
 }
